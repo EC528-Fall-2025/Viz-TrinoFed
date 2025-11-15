2a290b4d8295b024e50893c4e346739f01b12261
quargsgreene <quargsgreene@gmail.com>
2025-11-15 14:26:55 -0500

    removed Cypress files and uninstalled Cypress

--- src/components/NodesetStatusIcon.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { setStatusIcon } from './Node'
-
-describe('<setStatusIcon />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<setStatusIcon />)
-  })
-})
